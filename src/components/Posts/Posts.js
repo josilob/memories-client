b17b0fe52f2b8620cc8825e7685d3da9bd3cc50d
josilob <bjosilo@gmail.com>
2021-04-07 14:53:51 -0400

    Single post layout

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
@@ -7,12 +8,20 @@ const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
 	console.log(posts);
-	return (
-		<React.Fragment>
-			<div>POSTS</div>
-			<Post />
-			<Post />
-		</React.Fragment>
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.container}
+			container
+			alignItems='stretch'
+			spacing={3}>
+			{posts.map((post) => (
+				<Grid item key={post._id} xs={12} sm={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
