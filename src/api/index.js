035d7d3c3cb4414d67f3dd2c5b3b6d62f9b9cce8
josilob <bjosilo@gmail.com>
2021-04-15 18:33:37 +0200

    environmental vars

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://memories-rdx.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
-export const createPost = (newPost) => {
+export const createPost = (newPost) => { 
 	axios.post(url, newPost);
 };
 export const updatePost = (id, updatedPost) =>
