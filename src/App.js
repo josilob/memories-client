10b169a0f3623bd9f336afa558265e0c5142cc71
josilob <bjosilo@gmail.com>
2021-04-19 18:03:55 +0200

    conditional navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
