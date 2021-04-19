8cd10a65c84df37b0d4c5d474138072aedf6e5b6
josilob <bjosilo@gmail.com>
2021-04-19 17:50:19 +0200

    navbar modularized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/memories.png';
+import Navbar from './components/Navbar/Navbar';
 import useStyles from './styles';
 
 const App = () => {
@@ -19,12 +19,7 @@ const App = () => {
 
 	return (
 		<Container maxwidt='lg'>
-			<AppBar className={classes.appBar} position='static' color='inherit'>
-				<Typography className={classes.heading} variant='h2' align='center'>
-					Memories
-				</Typography>
-				<img className={classes.image} src={memories} alt='memories' height='60' />
-			</AppBar>
+			<Navbar />
 			<Grow in>
 				<Container>
 					<Grid
