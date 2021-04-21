a79762c50bee95c0e2d2b4374a725259db70f71d
josilob <bjosilo@gmail.com>
2021-04-21 23:47:43 +0200

    auth component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import {
+	Avatar,
+	Paper,
+	Button,
+	Grid,
+	Typography,
+	Container,
+} from '@material-ui/core';
+import LockOutlineIcon from '@material-ui/icons/LockOutlined';
+
+import useStyles from './styles';
 
 const Auth = () => {
-	return <div>Auth</div>;
+	const classes = useStyles();
+	const isSignup = false;
+	const handleSubmit = () => {};
+	const handleChange = () => {};
+
+	return (
+		<Container component='main' maxWidth='xs'>
+			<Paper className={classes.paper} elevation={3}>
+				<Avatar className={classes.avatar}>
+					<LockOutlineIcon />
+				</Avatar>
+				<Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+				<form className={classes.form} onSubmit={handleSubmit}>
+					<Grid container spacing={2}>
+						{isSignup && (
+							<>
+								<TextField
+									name='firstName'
+									label='First Name'
+									handleChange={handleChange}
+									autoFocus
+									xs={6}
+								/>
+								<TextField
+									name='firstName'
+									label='First Name'
+									handleChange={handleChange}
+									autoFocus
+									xs={6}
+								/>
+							</>
+						)}
+					</Grid>
+				</form>
+			</Paper>
+		</Container>
+	);
 };
 
 export default Auth;
